c7538ae91a9e46d521c5d4a0c490f440e2467bda
Tokiosman80F <tokiosman96@gmail.com>
2026-01-29 18:50:28 +0600

    fixed : js swiper and orbit for responsive

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 1,
-  loop: true,
-  speed: 600,
+// const swiper = new Swiper(".swiper", {
+//   slidesPerView: 1,
+//   loop: true,
+//   speed: 600,
 
-  navigation: {
-    nextEl: ".btn-right .nav-btn",
-    prevEl: ".btn-left .nav-btn",
-  },
+//   navigation: {
+//     nextEl: ".btn-right .nav-btn",
+//     prevEl: ".btn-left .nav-btn",
+//   },
 
-  pagination: {
-    el: ".dots",
-    clickable: true,
-    bulletClass: "dot",
-    bulletActiveClass: "active",
-  },
-});
+//   pagination: {
+//     el: ".dots",
+//     clickable: true,
+//     bulletClass: "dot",
+//     bulletActiveClass: "active",
+//   },
+// });
